164d526421fd39ae809b0091b0db44af4880e701
Yun0921 <yunyu.becky.liu@gmail.com>
2025-03-29 23:19:47 +0800

    Simpson n = 4

--- HW4/4-3-a.cpp
+++ HW4/4-3-a.cpp
@@ -17,7 +17,7 @@ double F(double x){
     double y2 = sin(x)+2*k(x);
     double y3 = sin(x)+3*k(x);
     double y4 = sin(x)+4*k(x);
-    double Fx = k(x)*(f(x, y0) + 4*f(x, y1) + 2*f(x, y2) + 4*f(x, y3) + f(x, y4));
+    double Fx = k(x)*(7*f(x, y0) + 32*f(x, y1) + 12*f(x, y2) + 32*f(x, y3) + 7*f(x, y4));
 
     return Fx;
 }
@@ -31,7 +31,7 @@ int main(){
     double x4 = M_PI/4;
     double h = (x4-x0)/4;
     double integral = 0;
-    integral = 1.0/9 * M_PI/16 * (F(x0) + 4*F(x1) + 2*F(x2) + 4*F(x3) + F(x4));
+    integral = pow(2.0/45, 2) * M_PI/16 * (7*F(x0) + 32*F(x1) + 12*F(x2) + 32*F(x3) + 7*F(x4));
     cout << "The integral of f(x) = 2y*sin(x) + cos(x)*cos(x) using Simpson's rule: " << integral << endl;
 
     double exact_integral = 0.511845;
